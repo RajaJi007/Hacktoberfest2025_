781923737c62ed858303bfa44e1d1aea6b9fcdfa
Harsh Raja <135194537+RajaJi007@users.noreply.github.com>
2025-10-29 20:02:04 +0530

    Add pattern printing with center plus

---
+++ X_patter_with_centre_plus.py
@@ -0,0 +1,7 @@
+for i in range(n):
+    for j in range(n):
+        if i == j or j == n - i - 1 or i == n // 2 or j == n // 2:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
